3aa6fa63b7fb368b135a83cdd6174e110ce3b2db
Francis Yankey <franciskobby@hotmail.com>
2022-08-04 20:19:55 +0000

    Pending Test

--- Data/enlighteddbContext.cs
+++ Data/enlighteddbContext.cs
@@ -25,7 +25,7 @@ namespace EnlightedApiConsumer.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;user=root;password=Inimitable@ROAD2020;database=enlighteddb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.21-mysql"));
+                optionsBuilder.UseMySql("server=localhost;user=root;password=;database=enlighteddb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.21-mysql"));
             }
         }
 
