118bf46926f6e1ff80a7e3fa53d198ad13e54f02
Francis Yankey <franciskobby@hotmail.com>
2022-08-05 07:30:15 +0000

    disposing database resource after use

--- Program.cs
+++ Program.cs
@@ -25,39 +25,48 @@ namespace EnlightedApiConsumer
         static async Task Main(string[] args)
         {
             Console.WriteLine("Initializing Configurations...");
+            
             IConfiguration _configuration = InitializeConfig();
 
             string _baseUrl = _configuration["BaseUrl"];
             string floorsEndPoint = _configuration["FloorsEndPoint"];
             string fixtureEndPoint = _configuration["FixtureEndPoint"];
             string connectionString = _configuration["connectionstring"];
-            enlighteddbContext _dbContext = new enlighteddbContext(connectionString);
-
-
-            IRequestHandler requestHandler = new RequestHandler(_baseUrl);
-
-            /*Test settings for BOB's example*/
-            //string date_string = "3/3/2016 7:36:51.032 PM";
-            //string username = "bob";
-            //string apiKey = "6eb6f07fd09b18dd61dd353dfb669820e7859cd3";
-
-            /*Live settings*/
-            string date_string = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt");
-            string username = _configuration["Username"];
-            string apiKey = _configuration["ApiKey"];
-
-            long currentTimeStamp = AuthorizationHandler.GetTimeStamp(date_string);
-            string authorizationHash = AuthorizationHandler.GetAuthorizationHash(username, apiKey, currentTimeStamp);
-
-            Console.WriteLine($"Authenticating as {username}");
             
-            Console.WriteLine("Starting Database Population...");
+            /*This block discards the database context after use*/
+            using (enlighteddbContext _dbContext = new enlighteddbContext(connectionString))
+            {
+                if (_dbContext.Database.CanConnect())
+                {
+                    IRequestHandler requestHandler = new RequestHandler(_baseUrl);
 
-            KeyValuePair<bool,string> databasePopulationResult = await DbWorker.PopulateDatabase(_dbContext, floorsEndPoint, fixtureEndPoint, requestHandler, username, currentTimeStamp, authorizationHash);
+                    /*Test settings for BOB's example*/
+                    //string date_string = "3/3/2016 7:36:51.032 PM";
+                    //string username = "bob";
+                    //string apiKey = "6eb6f07fd09b18dd61dd353dfb669820e7859cd3";
 
-           
+                    /*Live settings*/
+                    string date_string = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt");
+                    string username = _configuration["Username"];
+                    string apiKey = _configuration["ApiKey"];
 
-           
+                    long currentTimeStamp = AuthorizationHandler.GetTimeStamp(date_string);
+                    string authorizationHash = AuthorizationHandler.GetAuthorizationHash(username, apiKey, currentTimeStamp);
+
+                    Console.WriteLine($"Authenticating as {username}");
+
+                    Console.WriteLine("Starting Database Population...");
+
+                    KeyValuePair<bool, string> databasePopulationResult = await DbWorker.PopulateDatabase(_dbContext, floorsEndPoint, fixtureEndPoint, requestHandler, username, currentTimeStamp, authorizationHash);
+
+                }
+                else
+                {
+                    Console.WriteLine("Cannot connect to database. Kindly be sure that your connection \nstring or it's parameters(username,password,etc) are correct!");
+                }
+
+            }
+
         }
 
        
